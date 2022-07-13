1821d47dcb90233a219d7d68524d01f61c5e8a89
Axiera <axiera7734@gmail.com>
2022-07-13 09:00:48 +0300

    added textarea

---
+++ headers/widgets/textarea.h
@@ -0,0 +1,36 @@
+#ifndef TEXTAREA_H
+#define TEXTAREA_H
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+
+#define TEXTAREA_TEXT_COLOR 0, 0, 0, 255
+#define FONT_PATH "C:/Windows/Fonts/Arial.ttf"
+
+typedef struct {
+    SDL_Texture* text_texture;
+    TTF_Font* font;
+    int w, h;
+} textarea_t;
+
+textarea_t* textarea_init(int font_size);
+void textarea_deinit(textarea_t* textarea);
+void textarea_draw(const textarea_t* textarea,
+                   SDL_Renderer* renderer,
+                   int x,
+                   int y,
+                   int h);
+void textarea_set_text(textarea_t* textarea,
+                       SDL_Renderer* renderer,
+                       const char* text,
+                       int w);
+
+/*
+    SDL ttf must be initialized
+
+    textarea_init()
+        returns pointer to textarea_t on success
+        returns NULL on error, call SDL_GetError() for more information
+*/
+
+#endif
---
+++ sources/widgets/textarea.c
@@ -0,0 +1,64 @@
+#include "../../headers/widgets/textarea.h"
+
+textarea_t* textarea_init(int font_size) {
+    textarea_t* textarea = malloc(sizeof(textarea_t));
+    if (textarea == NULL) {
+        SDL_SetError("memory allocation failed\n%s()", __func__);
+        return NULL;
+    }
+
+    textarea->font = TTF_OpenFont(FONT_PATH, font_size);
+    if (textarea->font == NULL) {
+        free(textarea);
+        SDL_SetError("can not open %s\n%s()", FONT_PATH, __func__);
+        return NULL;
+    }
+
+    return textarea;
+}
+
+void textarea_deinit(textarea_t* textarea) {
+    TTF_CloseFont(textarea->font);
+    SDL_DestroyTexture(textarea->text_texture);
+    free(textarea);
+}
+
+void textarea_draw(const textarea_t* textarea,
+                   SDL_Renderer* renderer,
+                   int x,
+                   int y,
+                   int h) {
+    SDL_Rect srcrect = {
+        .x = 0,
+        .y = 0,
+        .w = textarea->w,
+        .h = textarea->h <= h ? textarea->h : h
+    };
+    SDL_Rect dstrect = {
+        .x = x,
+        .y = y,
+        .w = srcrect.w,
+        .h = srcrect.h
+    };
+    SDL_RenderCopy(renderer, textarea->text_texture, &srcrect, &dstrect);
+}
+
+void textarea_set_text(textarea_t* textarea,
+                       SDL_Renderer* renderer,
+                       const char* text,
+                       int w) {
+    SDL_Surface* surface = TTF_RenderUTF8_Blended_Wrapped(
+        textarea->font,
+        text,
+        (SDL_Color){ TEXTAREA_TEXT_COLOR },
+        w
+    );
+    if (surface == NULL)
+        return;
+    textarea->w = surface->w;
+    textarea->h = surface->h;
+    if (textarea->text_texture != NULL)
+        SDL_DestroyTexture(textarea->text_texture);
+    textarea->text_texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+}
